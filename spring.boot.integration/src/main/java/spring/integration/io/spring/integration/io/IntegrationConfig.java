fa300e7f95a445356f7906567689eba67f546165
umeshbatra <umeshbatra@umeshs-air>
2019-03-27 01:38:34 +0530

    Commit

---
+++ spring.boot.integration/src/main/java/spring/integration/io/spring/integration/io/IntegrationConfig.java
@@ -0,0 +1,47 @@
+package spring.integration.io.spring.integration.io;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+
+@Configuration
+public class IntegrationConfig {
+	
+	@Autowired
+	private Transformer transformer;
+	
+	@Bean
+	public IntegrationFlow integrationFlow() {
+		
+		return IntegrationFlows.from(fileReader(), 
+				spec -> spec.poller(Pollers.fixedDelay(500))).
+				transform(transformer,"transform").
+				handle(fileWriter()).get();
+	}
+	
+	@Bean
+	public FileWritingMessageHandler fileWriter() {
+
+		FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("Destination"));
+		handler.setExpectReply(false);
+		return handler;
+		
+	}
+	
+	@Bean
+	public FileReadingMessageSource fileReader() {
+
+		FileReadingMessageSource source = new FileReadingMessageSource();
+		source.setDirectory(new File("source"));
+		return source;
+		
+	}
+
+}
---
+++ spring.boot.integration/src/main/java/spring/integration/io/spring/integration/io/Transformer.java
@@ -0,0 +1,17 @@
+package spring.integration.io.spring.integration.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Transformer {
+
+	public String transform(String filePath) throws IOException {
+		
+		String content = new String(Files.readAllBytes(Paths.get(filePath)));
+		return "Transformed: " + content;
+	}
+}
